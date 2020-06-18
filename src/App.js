25a0b2a2df940c3520cebc238ee6aca1d59e0cca
Jason Hinton <jason_hinton@att.net>
2020-06-17 22:20:25 -0400

    initial

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Switch>
+        <Route></Route>
+      </Switch>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
