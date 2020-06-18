b2565b13ece0ddb65ee6602aff79cab33d0b52de
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-06-17 20:43:43 -0600

    bare-bones react project set up

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {  } from 'react';
+import './App.css';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  background-color: black;
+`;
+
+function App() {
+  return (
+    <Wrapper className="App">
+    
+      <p>Hello
+      </p>
+    </Wrapper>
+  );
+}
+
+export default App;
