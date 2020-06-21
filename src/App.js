537b2e3bdc4e7f81212cc7d64fa1b5dfb811d1ac
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-06-20 20:48:18 -0600

    updating discrepancies between My version & Jason's version

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ const Wrapper = styled.div`
 `;
 
 
+
 function App() {
 	const [track, setTrack] = useState([]);
 
@@ -39,28 +40,14 @@ function App() {
 	return (
 		<Router>
 			<Wrapper className="App">
-<<<<<<< HEAD
-				<NavBar />
-				<Switch>
-					<Route exact path="/" component={SignIn}></Route>
-					<Route path='/songs' component={Songs}></Route>
-					<Route path='/about' component={About}></Route>
-				</Switch>
-=======
 				<TrackContext.Provider value={{ track }}>
 					<NavBar />
-					<Banner>
-						<h2 id="app-callout">
-							We find the music so all you have to do is enjoy!
-						</h2>
-					</Banner>
 					<Switch>
 						<Route exact path="/" component={SignIn}></Route>
 						<Route path="/songs" component={Songs}></Route>
 						<Route path="/about" component={About}></Route>
 					</Switch>
 				</TrackContext.Provider>
->>>>>>> 3fc0c8de47b7660e04fbc9019a27d309c4dd4de1
 			</Wrapper>
 		</Router>
 	);
